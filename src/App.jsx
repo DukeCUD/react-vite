65b997b77afb3c582f44f3c83ff9205b7a424ef3
ndDuke <104844845+ndDuke@users.noreply.github.com>
2024-07-13 07:35:49 +0700

    vd29

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import MyComponent from "./components/MyComponent";
+import { SecondComponent, ThirdComponent } from "./components/SecondComponent";
 
-const MyComponent = () => {
-  return (
-    <div>
-      <p>My component update file </p>
-    </div>
-  );
-};
 const App = () => {
   const [count, setCount] = useState(0);
 
@@ -25,6 +20,8 @@ const App = () => {
       </div>
       <h1>Hello world</h1>
       <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/MyComponent.jsx
@@ -0,0 +1,9 @@
+const MyComponent = () => {
+  return (
+    <div>
+      <p>My component update file </p>
+    </div>
+  );
+};
+
+export default MyComponent;
