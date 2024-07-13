73b22ed6fcfc617088997afc5b412da02e30a528
ndDuke <104844845+ndDuke@users.noreply.github.com>
2024-07-13 08:40:03 +0700

    vd31

--- src/components/MyComponent.jsx
+++ src/components/MyComponent.jsx
@@ -1,9 +1,11 @@
 import "./style.css";
 
 const MyComponent = () => {
+  const input = [1, 2, 3];
   return (
     <>
-      <p className="cp1">My component update file </p>
+      <div className="cp1">{input} My component update filal </div>
+      <div>{console.log("Nguyen Hong Duc")} </div>
       <div className="cp2" style={{ color: "green" }}>
         Component first one
       </div>
@@ -14,3 +16,5 @@ const MyComponent = () => {
 export default MyComponent;
 // Dùng để xuất ra duy nhất 1 biến
 // JSX chỉ có thể chứa 1 phần tử
+// {:dùng JS bên trong HTML{:object}}
+// Nếu muốn convert array, object từ dạng chuỗi JSON.stringify(tên array,object)
