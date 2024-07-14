f4ae1affba95b5b695a654818fc1233431cf20e8
ndDuke <104844845+ndDuke@users.noreply.github.com>
2024-07-15 01:20:59 +0700

    vd45 addReactRounter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/login",
+    element: <div>Login page</div>,
+  },
+  {
+    path: "/register",
+    element: <div>Regester page</div>,
+  },
+  {
+    path: "/use",
+    element: <div>User page</div>,
+  },
+  {
+    path: "/product",
+    element: <div>Product page</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
